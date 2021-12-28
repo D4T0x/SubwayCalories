a937fab67d3822587052b7d741a680ed6aaba1c8
D4T0x <enrique.sanz.gil@alumnos.upm.es>
2021-12-28 18:41:34 +0100

    a

--- Vista.py
+++ Vista.py
@@ -1,10 +1,10 @@
 from tkinter import *
 
 def run1():
-    kcal = StringVar(e1.get())
-    a = '%i\n' (kcal)
+    kcal = int(e1.get())
+    a = '%i\n' % (kcal)
     txt.insert(END, a)
-
+    e1.delete(0, END)
 
 
 # Definimos la ventana
@@ -14,16 +14,18 @@ ventana.geometry('460x240')
 
 
 # Definimos la entrada
-lb = Label(ventana, text='Ingrese las kilocalorias a pedir y presione un boton')
+lb = Label(ventana, text='Ingrese las kilocalorias a pedir y presione el boton')
 lb.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
 e1 = Entry(ventana, show=None)
-e1.place(relx=0.1, rely=0.2, relwidth=0.3, relheight=0.1)
+e1.place(relx=0.35, rely=0.2, relwidth=0.3, relheight=0.1)
 
-btn = Button(ventana, text='Ingrese kcal', command=run1)
-btn.place(relx=0.1, rely=0.4, relwidth=0.3, relheight=0.1)
+btn = Button(ventana, text='Run!', command=run1)
+btn.place(relx=0.35, rely=0.4, relwidth=0.3, relheight=0.1)
 
 txt = Text(ventana)
 txt.place(rely=0.6, relheight=0.4)
 
 ventana.mainloop()
+
+
 
